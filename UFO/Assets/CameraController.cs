7f8d1abeeed386734d9261a803f107aaf6ec6774
AngryCracker <ads6may@gmail.com>
2017-05-28 22:05:31 +0530

    Added camera transform

---
+++ UFO/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //LateUpdate runs every frame but only after all items have been processed in Update
+        transform.position = player.transform.position + offset;
+	}
+}
