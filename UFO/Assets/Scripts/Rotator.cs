22e251933e418fa5b3668056cfbf3f3d27555c9b
AngryCracker <ads6may@gmail.com>
2017-05-28 22:24:12 +0530

    Added pickups and moved scripts to a new folder

---
+++ UFO/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        //multiplied with deltaTime so that rotation is smooth and independent of framerate
+	}
+}
