72dc8458795af58468aa497c57ee40bfc072633f
AngryCracker <ads6may@gmail.com>
2017-05-28 23:06:33 +0530

    Added counter text

--- UFO/Assets/Scripts/PlayerController.cs
+++ UFO/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb2d;
+    private int count;
     public float speed;
+    public Text counttext;
+    public Text wintext;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        wintext.text = "";
 	}
 	
 	// Update is called once per frame
@@ -30,6 +37,17 @@ public class PlayerController : MonoBehaviour {
         if (collision.gameObject.CompareTag("PickUp"))
         {
             collision.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        counttext.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            wintext.text = "You Win!";
         }
     }
 }
