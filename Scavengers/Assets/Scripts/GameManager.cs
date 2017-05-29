5e2c9b2a492ea7ff5f9995f803aa588289760758
AngryCracker <ads6may@gmail.com>
2017-05-29 13:09:36 +0530

    Populated arrays

--- Scavengers/Assets/Scripts/GameManager.cs
+++ Scavengers/Assets/Scripts/GameManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public BoardManager boardscript;
+    private int level = 3;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        boardscript = GetComponent<BoardManager>();
+        InitGame();
 	}
+
+    void InitGame()
+    {
+        boardscript.SetupScene(level);
+    }
 	
 	// Update is called once per frame
 	void Update () {
