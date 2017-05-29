672b5ef8047deba83d4e0bad67d23e8918bf4ab4
AngryCracker <ads6may@gmail.com>
2017-05-29 13:59:00 +0530

    Added damaged sprites

---
+++ Scavengers/Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
