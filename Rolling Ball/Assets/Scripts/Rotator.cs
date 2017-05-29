5c9112d23d32ebbf33afa2eae57d1459438e6d03
AngryCracker <ads6may@gmail.com>
2017-05-29 23:25:40 +0530

    Added pickups

---
+++ Rolling Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        //We multiply by Time.deltaTime so that the rotation is smooth and framerate independent
+	}
+
+    //Since we are not updating physics, we will not use FixedUpdate()
+}
