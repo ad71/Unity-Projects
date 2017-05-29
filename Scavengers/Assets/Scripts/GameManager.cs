1d37d00f4f7c8e6f0c02cf1a789fcb2ff042d329
AngryCracker <ads6may@gmail.com>
2017-05-29 16:43:32 +0530

    GameManager edit

--- Scavengers/Assets/Scripts/GameManager.cs
+++ Scavengers/Assets/Scripts/GameManager.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    public float levelStartDelay = 2f;
     public float turnDelay = 0.1f;
     public static GameManager instance = null;
     public BoardManager boardscript;
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playersTurn = true;
 
-    private int level = 3;
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
     private List<Enemy> enemies;
     private bool enemiesMoving;
-
+    private bool doingSetup;
 
 	// Use this for initialization
 	void Awake () {
@@ -32,12 +36,32 @@ public class GameManager : MonoBehaviour {
 
     void InitGame()
     {
+        doingSetup = true;
+        levelImage = GameObject.Find("levelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
         enemies.Clear();
         boardscript.SetupScene(level);
     }
-	
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+
     public void GameOver()
     {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
         enabled = false;
     }
 
@@ -58,7 +82,7 @@ public class GameManager : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-        if (playersTurn || enemiesMoving)
+        if (playersTurn || enemiesMoving || doingSetup)
             return;
 
         StartCoroutine(MoveEnemies());
