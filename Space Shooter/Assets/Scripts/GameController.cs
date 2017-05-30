eccd569b605088b883c2152a9574eb2630c59c9e
AngryCracker <ads6may@gmail.com>
2017-05-30 17:39:12 +0530

    Added script to control spawning of hazards

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+	// Use this for initialization
+	void Start () {
+        SpawnWaves();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0, spawnValues.z);
+        Quaternion spawnRotation = new Quaternion();
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
