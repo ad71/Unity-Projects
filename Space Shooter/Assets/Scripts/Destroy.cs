a43ff7db98f816caacc16cb771e7f1b87a3709e5
AngryCracker <ads6may@gmail.com>
2017-05-30 16:58:21 +0530

    Added asteroids and collision detection

---
+++ Space Shooter/Assets/Scripts/Destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+            return;
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+        //All destroy calls are stacked and executed at the end of each frame together, so the order of destruction doesn't matter
+    }
+}
---
+++ Space Shooter/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float tumble;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
