e587d84ea988398619c4caade78f8198e207526c
AngryCracker <ads6may@gmail.com>
2017-05-31 00:33:50 +0530

    Added rotation

--- Car 2D/Assets/Scripts/CarController.cs
+++ Car 2D/Assets/Scripts/CarController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// The line below is going to force Unity to require a riidbody2d on the car controller
+// [RequireComponent(typeof(Rigidbody2D))]
 public class CarController : MonoBehaviour {
 
     public float speed = 1500;
+    public float rotationspeed = 15f;
     public WheelJoint2D backWheel;
     public WheelJoint2D frontWheel;
+    public Rigidbody2D rb;
     private float movement = 0;
+    private float tilt = 0;
     private void Update()
     {
+        // rb = GetComponent<Rigidbody2D>();
+        // GetAxis smoothens user input which feels a bit 'nice' but not very respondent
         movement = -Input.GetAxisRaw("Vertical") * speed;
+        tilt = Input.GetAxisRaw("Horizontal");
     }
 
     private void FixedUpdate()
@@ -32,5 +40,7 @@ public class CarController : MonoBehaviour {
             backWheel.motor = motor;
             frontWheel.motor = motor;
         }
+        rb.AddTorque(-tilt * rotationspeed * Time.fixedDeltaTime);
+        // Maybe fixedDeltaTime adds framerate independence
     }
 }
