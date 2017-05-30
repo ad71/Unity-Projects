bb9fc047583d80912d3b71eeedd7eccb9b4be3bf
AngryCracker <ads6may@gmail.com>
2017-05-30 19:01:33 +0530

    Added sounds

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour {
         if(Input.GetButton("Fire1") && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotspawn.position, shotspawn.rotation);
+            GetComponent<AudioSource>().Play();
         }
 	}
 
