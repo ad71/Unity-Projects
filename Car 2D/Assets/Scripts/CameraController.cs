815b039f0d114b24e8d291af125b716652685f36
AngryCracker <ads6may@gmail.com>
2017-05-31 00:42:12 +0530

    Added camera position transform

---
+++ Car 2D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 newposition = target.position;
+        newposition.z = -10;
+        this.transform.position = newposition;
+    }
+}
