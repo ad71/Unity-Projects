41cdcbcde8411572d38dfe6e6fcd8e2095d82e92
AngryCracker <ads6may@gmail.com>
2017-05-31 01:11:02 +0530

    Added goal

---
+++ Car 2D/Assets/GoalComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoalComponent : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")) {
+            Debug.Log("GAME WON! :D");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
