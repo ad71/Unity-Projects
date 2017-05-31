25eb52eff335da3383972e33d022b6a4a7c1bc95
AngryCracker <ads6may@gmail.com>
2017-05-31 22:28:52 +0530

    Complete. Didn't add UI.

--- AA/Assets/GameManager.cs
+++ AA/Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -20,5 +21,11 @@ public class GameManager : MonoBehaviour {
         spawner.enabled = false;
         // animator.SetTrigger("EndGame");
         gameHasEnded = true;
+        RestartLevel();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
