a786d01d39a73af5b7f89c8c92aa4b7d43639b27
AngryCracker <ads6may@gmail.com>
2017-05-31 21:28:12 +0530

    Basic game mechanics worked out

---
+++ AA/Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject pin;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1"))
+        {
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin()
+    {
+        Instantiate(pin, this.transform.position, this.transform.rotation);
+    }
+}
