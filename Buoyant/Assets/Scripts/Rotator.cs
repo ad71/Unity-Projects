5cab4b017c7b543aca0e682e7b020532bcb87993
AngryCracker <ads6may@gmail.com>
2017-06-01 00:35:01 +0530

    Very unrealistic

---
+++ Buoyant/Assets/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float torque;
+    public float x;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // rb.AddTorque(torque);
+        rb.AddForce(new Vector2(x, 0));
+	}
+}
