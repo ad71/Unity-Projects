0e71ba09bac64d773da5fc870b7a3d6d5b6e83b0
AngryCracker <ads6may@gmail.com>
2017-05-31 20:56:50 +0530

    Added gameobjects and physics components

---
+++ AA/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed = 100f;
+
+    private void Update()
+    {
+        this.transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        // We multiply this by Time.deltaTime because we are using the Update methid and we want it to be framerate independent
+    }
+}
