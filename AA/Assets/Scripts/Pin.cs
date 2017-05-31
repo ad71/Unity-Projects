0289bb1c5ab3b4da074bed1facdd1ac56472678f
AngryCracker <ads6may@gmail.com>
2017-05-31 21:16:33 +0530

    Minor edit

---
+++ AA/Assets/Scripts/Pin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    private bool isPinned = false;
+
+    private void Update()
+    {
+        if(!isPinned)
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+        // The above function allows us to move the rigidbody while still checking for physics
+        // A lot better to use than transform.translate because it won't work well with collisions
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Rotator"))
+        {
+            isPinned = true;
+        }
+    }
+}
