4d53890ef0cdd106ed4b98def91f18b2aa6c528f
AngryCracker <ads6may@gmail.com>
2017-05-31 20:06:55 +0530

    Complete

---
+++ Line Rider/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newpos = target.position;
+        newpos.z = -10;
+        this.transform.position = newpos;
+	}
+}
