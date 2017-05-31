1ddf321156db03f0784f2b2565b99dd379914076
AngryCracker <ads6may@gmail.com>
2017-05-31 19:52:11 +0530

    Added materials and line varieties

---
+++ Line Rider/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    private void Update()
+    {
+        if(Input.GetButtonDown("Start"))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
