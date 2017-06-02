786f95095f0dd58ddf217330ac08fecdf9df4947
AngryCracker <ads6may@gmail.com>
2017-06-03 00:50:07 +0530

    Wheels turn and snap back when released. Vehicle doesn't turn yet

--- Top Down Car 2/Assets/Scripts/BackWheelController.cs
+++ Top Down Car 2/Assets/Scripts/BackWheelController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelController : MonoBehaviour {
+public class BackWheelController : MonoBehaviour {
 
     private Rigidbody2D rb;
     public float speed = 10f;
---
+++ Top Down Car 2/Assets/Scripts/FrontWheelController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontWheelController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private HingeJoint2D hj;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        hj = GetComponent<HingeJoint2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        JointAngleLimits2D anglesx = new JointAngleLimits2D
+        {
+            min = -30,
+            max = 30
+        };
+        hj.limits = anglesx;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            hj.useMotor = true;
+            JointMotor2D motor = new JointMotor2D
+            {
+                motorSpeed = 100,
+                maxMotorTorque = 10000
+            };
+            hj.motor = motor;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            hj.useMotor = true;
+            JointMotor2D motor = new JointMotor2D
+            {
+                motorSpeed = -100,
+                maxMotorTorque = 10000
+            };
+            hj.motor = motor;
+        } 
+        else
+        {
+            JointAngleLimits2D angles = new JointAngleLimits2D
+            {
+                min = 0,
+                max = 0
+            };
+            hj.limits = angles;
+        }
+	}
+}
