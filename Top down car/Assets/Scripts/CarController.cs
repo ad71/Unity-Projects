d6845f3628623d4085b96cf962a343615c225502
AngryCracker <ads6may@gmail.com>
2017-06-02 14:18:03 +0530

    Corrected reverse turning

--- Top down car/Assets/Scripts/CarController.cs
+++ Top down car/Assets/Scripts/CarController.cs
@@ -8,6 +8,7 @@ public class CarController : MonoBehaviour {
     public float torque = -500f;
     public float driftFactor = 1f;
     private Rigidbody2D rb;
+    private bool movingForward;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
@@ -18,16 +19,22 @@ public class CarController : MonoBehaviour {
         Vector2 toAdd = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            movingForward = true;
             rb.AddForce(this.transform.up * speed);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
+            movingForward = false;
             rb.AddForce(-this.transform.up * speed);
         }
         float vf = getVelocityTangent().magnitude;
-        if (vf > 0.5f) { 
+        if (vf > 0.5f && movingForward) { 
             rb.angularVelocity = Input.GetAxis("Horizontal") * torque;
             toAdd = new Vector2(Input.GetAxis("Horizontal")/10, 0);
+        } else if (vf > 0.5f && !movingForward)
+        {
+            rb.angularVelocity = -Input.GetAxis("Horizontal") * torque;
+            toAdd = new Vector2(-Input.GetAxis("Horizontal") / 10, 0);
         }
                 
         // rb.AddTorque(Input.GetAxis("Horizontal") * torque);
