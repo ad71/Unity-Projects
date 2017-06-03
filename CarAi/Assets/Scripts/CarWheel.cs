899f4a755e2ec6ecbe5beb5944fc0449150acf91
AngryCracker <ads6may@gmail.com>
2017-06-03 21:25:33 +0530

    Tires rotate with wheelcolliders

---
+++ CarAi/Assets/Scripts/CarWheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWheel : MonoBehaviour {
+
+    public WheelCollider target;
+    private Vector3 wheelposition = new Vector3();
+    private Quaternion wheelrotation = new Quaternion();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target.GetWorldPose(out wheelposition, out wheelrotation);
+        this.transform.position = wheelposition;
+        this.transform.rotation = wheelrotation;
+	}
+}
