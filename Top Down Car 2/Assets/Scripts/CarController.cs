1a3dcc47f75a179551c400854f9bfbecf03f59c8
AngryCracker <ads6may@gmail.com>
2017-06-03 16:34:28 +0530

    RayCastHit2D working

--- Top Down Car 2/Assets/Scripts/CarController.cs
+++ Top Down Car 2/Assets/Scripts/CarController.cs
@@ -6,13 +6,26 @@ public class CarController : MonoBehaviour {
 
     private Rigidbody2D rb;
     public float brakeforce = 10f;
+    public Camera camera;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        Vector3 screenPos = camera.ScreenToWorldPoint(mousePos);
+        RaycastHit2D hit = Physics2D.Raycast(screenPos, Vector2.zero);
+
+        if(hit)
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
         if (Input.GetKey(KeyCode.DownArrow))
             rb.AddForce(-this.transform.up * brakeforce);
 	}
