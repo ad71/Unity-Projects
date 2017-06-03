887cc704a5318c8051b72459df6721ab0b65e812
AngryCracker <ads6may@gmail.com>
2017-06-03 21:13:59 +0530

    Finds relative vector to waypoint and sets angles of wheelcolliders

---
+++ CarAi/Assets/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+    public Transform path;
+    public float maxSteerAngle = 45f;
+    public WheelCollider wheelfl;
+    public WheelCollider wheelfr;
+
+    private List<Transform> nodes;
+    private int current = 0;
+	// Use this for initialization
+	void Start () {
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for(int i = 0; i < pathTransforms.Length; ++i)
+        {
+            if (pathTransforms[i] != path.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+        ApplySteer();
+	}
+
+    private void ApplySteer()
+    {
+        Vector3 relative = this.transform.InverseTransformPoint(nodes[current].position);
+        // relative /= relative.magnitude; // can be done by relative.Normalize() probably :/
+        float steer = (relative.x / relative.magnitude) * maxSteerAngle;
+        wheelfl.steerAngle = steer;
+        wheelfr.steerAngle = steer;
+    }
+}
